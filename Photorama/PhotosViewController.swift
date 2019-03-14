87c8ef4fd4cae550b0cc0a2a1150a67fe115e4c7
Shubh Patel <patelshubh96@gmail.com>
2019-03-14 19:55:23 -0400

    Initial Commit

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Shubh Patel on 2019-03-14.
+//  Copyright © 2019 Shubh Patel. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
